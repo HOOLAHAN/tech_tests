79159a6a4d67f27b48356cfbb56b90e882f796c9
Iain <iain.hoolahan@gmail.com>
2022-12-12 18:26:35 +0000

    transaction class created and method for depositing an amount tested with time generated

---
+++ bank_tech_test_ruby/lib/transaction.rb
@@ -0,0 +1,19 @@
+# File: lib/transaction.rb
+
+class Transaction
+
+  def initialize(account)
+    @account = account
+  end
+
+  def get_date
+    return Time.now.strftime('%m/%d/%Y')
+  end
+
+  def deposit(ammount)
+    transaction = []
+    transaction << get_date << ammount
+    @account << transaction
+  end
+
+end
