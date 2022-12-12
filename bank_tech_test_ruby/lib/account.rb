7cd47dcd4f2bb5a739095b0cccfb3a7cc4cf5e68
Iain <iain.hoolahan@gmail.com>
2022-12-12 17:38:54 +0000

    first test on Account class passing - initialization of account and balance

---
+++ bank_tech_test_ruby/lib/account.rb
@@ -0,0 +1,18 @@
+# File: lib/account.rb
+
+class Account
+
+  def initialize
+    @balance = 0
+    @account = []
+  end
+
+  def account
+    return @account
+  end
+
+  def balance
+    return @balance
+  end
+
+end
