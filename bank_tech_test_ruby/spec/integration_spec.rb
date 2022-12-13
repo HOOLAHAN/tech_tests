574bf20cb93dbf644c0cdd6bb78b926d966e8f26
Iain <iain.hoolahan@gmail.com>
2022-12-13 16:14:01 +0000

    create_statement integration tested

--- bank_tech_test_ruby/spec/integration_spec.rb
+++ bank_tech_test_ruby/spec/integration_spec.rb
@@ -31,12 +31,36 @@ describe 'transaction - account class integration' do
       test_transaction_2 = Transaction.new.deposit(300)
       test_account.add_transaction(test_transaction_2)
 
+      test_transaction_3 = Transaction.new.withdraw(100)
+      test_account.add_transaction(test_transaction_3)
+
       test_account.add_balance
       result = test_account.show_account
 
       date_today = Time.now.strftime('%d/%m/%Y')
+      expect(result).to eq [[date_today, 200, 200], [date_today, 300, 500], [date_today, -100, 400]]
+    end
+  end
 
-      expect(result).to eq [[date_today, 200, 200], [date_today, 300, 500]]
+
+  context 'when transactions have been made on the account and the create_statement method is called' do
+    it 'it preapres a statement array showing the transactions and account balance formatted as required' do
+      test_account = Account.new
+
+      test_transaction_1 = Transaction.new.deposit(200)
+      test_account.add_transaction(test_transaction_1)
+
+      test_transaction_2 = Transaction.new.deposit(300)
+      test_account.add_transaction(test_transaction_2)
+
+      test_transaction_3 = Transaction.new.withdraw(100)
+      test_account.add_transaction(test_transaction_3)
+
+      prepared_account = test_account.add_balance
+      test_statement = Statement.new.create_statement(prepared_account)
+
+      date_today = Time.now.strftime('%d/%m/%Y')
+      expect(test_statement).to eq ["date || credit || debit || balance", "#{date_today} || || -100 || 400", "#{date_today} || 300 || || 500", "#{date_today} || 200 || || 200"]
     end
   end
 
