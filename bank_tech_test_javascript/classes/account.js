244baec6bfb370571952581bfba8afad5ef937ef
Iain <iain.hoolahan@gmail.com>
2023-01-31 11:21:12 +0000

    getBalance unit tested

--- bank_tech_test_javascript/classes/account.js
+++ bank_tech_test_javascript/classes/account.js
@@ -2,16 +2,23 @@ class Account {
 
   constructor() {
     this.account = [];
+    this.balance = 0;
   }
 
   getAccount() {
     return this.account
   }
 
-  addTransaction(transaction) {
-    this.account.push(transaction)
+  addTransaction(transactionObject) {
+    transactionObject.balance = this.getBalance(transactionObject)
+    this.account.push(transactionObject)
   }
-
+  
+  getBalance(transactionObject) {
+    this.balance = this.account.map(x => x.transaction).reduce((a, b) => a + b, 0) + transactionObject.transaction
+    return this.balance
+  }
+  
 }
 
 module.exports = Account;
--- bank_tech_test_javascript/tests/account.test.js
+++ bank_tech_test_javascript/tests/account.test.js
@@ -16,7 +16,31 @@ describe('Account', () => {
     testAccount = new Account;
     transactionDouble = { transaction: 500, date: '31/01/2023' }
     testAccount.addTransaction(transactionDouble)
-    expect(testAccount.account[0]).toEqual({ transaction: 500, date: '31/01/2023' })
+    expect(testAccount.account[0]).toEqual(expect.objectContaining({ transaction: 500, date: '31/01/2023' }))
+  })
+  
+  it('addBalance function adds a balance to the account array', () => {
+    testAccount = new Account;
+    transactionDouble1 = { transaction: 500, date: '29/01/2023' }
+    testAccount.addTransaction(transactionDouble)
+    expect(testAccount.account[0]).toEqual({ transaction: 500, date: '31/01/2023', balance: 500 })
+  })
+
+  it('multiple transactions can be added to the account array', () => {
+    testAccount = new Account;
+
+    transactionDouble1 = { transaction: 500, date: '29/01/2023' }
+    transactionDouble2 = { transaction: 600, date: '30/01/2023' }
+    transactionDouble3 = { transaction: -700, date: '31/01/2023' }
+
+    testAccount.addTransaction(transactionDouble1)
+    testAccount.addTransaction(transactionDouble2)
+    testAccount.addTransaction(transactionDouble3)
+
+    expect(testAccount.account[0]).toEqual({ transaction: 500, date: '29/01/2023', balance: 500 })
+    expect(testAccount.account[1]).toEqual({ transaction: 600, date: '30/01/2023', balance: 1100 })
+    expect(testAccount.account[2]).toEqual({ transaction: -700, date: '31/01/2023', balance: 400 })
+
   })
 
 })
