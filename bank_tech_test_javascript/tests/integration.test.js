d98ce9787d3990008a78ec3ad1246e7d11c7bfb0
Iain <iain.hoolahan@gmail.com>
2023-01-31 11:28:54 +0000

    addBalance integration tested

--- bank_tech_test_javascript/tests/integration.test.js
+++ bank_tech_test_javascript/tests/integration.test.js
@@ -13,4 +13,23 @@ describe('Integration', () => {
       expect.objectContaining({ transaction: -500 })
     )
   })
+
+  it('adds multiple transactions to the account with balance', () => {
+    let testAccount = new Account;
+    let testTransaction1 = new Transaction;
+    let testTransaction2 = new Transaction;
+
+    testTransaction1.deposit(600)
+    testTransaction2.withdraw(500)
+
+    testAccount.addTransaction(testTransaction1);
+    testAccount.addTransaction(testTransaction2);
+
+    expect(testAccount.account[0]).toEqual(
+      expect.objectContaining( { transaction: 600, balance: 600 } )
+    )
+    expect(testAccount.account[1]).toEqual(
+      expect.objectContaining( { transaction: -500, balance: 100 } )
+    )
+  })
 })
